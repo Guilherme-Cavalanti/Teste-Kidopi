495b82e20a3b40c4322e95576391e7ed764ab572
Guilherme-Cavalanti <guilhermecavalanti@gmail.com>
2024-04-12 21:35:47 -0300

    fix index.php script and style

--- Projeto-Kidopi/public/index.php
+++ Projeto-Kidopi/public/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="/public/css/style.css">
     <link   
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"
@@ -15,7 +15,7 @@
         
     </script>
     <title>Main Page</title>
-    <script src="./js/fetch.js">
+    <script src="/public/js/fetch.js">
     </script>
 </head>
 <body>
@@ -83,6 +83,6 @@
             </div>
         </div>
     </div>
-    <script src="./js/index.js"> </script>
+    <script src="/public/js/index.js"> </script>
 </body>
 </html>
